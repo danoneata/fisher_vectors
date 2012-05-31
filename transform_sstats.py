1bd386200add02446a6412302342daadc6354050
Dan Oneata <dan.oneata@inria.fr>
2012-05-31 11:35:03 +0200

    Added documentation and code for spatial Fisher vectors.

--- transform_sstats.py
+++ transform_sstats.py
@@ -7,14 +7,26 @@ from numpy import hstack, newaxis
 from yael import yael
 
 
-def sstats_to_fv(sstats, gmm):
-    """ Gets the statistics sstats and the Gaussian mixture model object gmm
-    and returns a NxD matrix containing the Fisher vectors.
+def sstats_to_fv(ss, gmm):
+    """ Converts sufficient statistics into standard Fisher vectors. 
+    
+    Inputs
+    ------
+    ss: array [N, K + 2 * D * K]
+        Sufficient statistics.
+
+    gmm: instance of the yael object, gmm
+        Gaussian mixture model.
+
+    Output
+    ------
+    fv: array [N, K + 2 * D * K]
+        Fisher vectors for each of the N sufficient statistics.
 
     """
     K = gmm.k
     D = gmm.d
-    ss = sstats.reshape(-1, K + 2 * K * D)
+    ss = ss.reshape(-1, K + 2 * K * D)
     N = ss.shape[0]
 
     # Get parameters and reshape them.
@@ -29,7 +41,7 @@ def sstats_to_fv(sstats, gmm):
     Q_xx = ss[:, K:K + D * K]                    # NxKD
     Q_xx_2 = ss[:, K + D * K:K + 2 * D * K]      # NxKD
 
-    # Compute derivatives.
+    # Compute the gradients.
     d_pi = Q_sum - pi
     d_mu = Q_xx - (Q_sum[:, newaxis] * mu).reshape(N, D * K, order='F')
     d_sigma = (
@@ -39,5 +51,71 @@ def sstats_to_fv(sstats, gmm):
         + 2 * Q_xx * mu.reshape(1, K * D, order='F'))
 
     # Glue everything together.
-    xx = hstack((d_pi, d_mu, d_sigma))
+    fv = hstack((d_pi, d_mu, d_sigma))
+    return fv 
+
+
+def sstats_to_soft_bow(ss, gmm):
+    """ Converts sufficient statistics into soft counts of bag-of-words, using
+    the posterior probabilities.
+
+    Inputs
+    ------
+    ss: array [N, K + 2 * D * K]
+        Sufficient statistics.
+
+    gmm: instance of the yael object, gmm
+        Gaussian mixture model.
+
+    Output
+    ------
+    soft_bow: array [N, K]
+        Soft counts for each of the N sufficient statistics.
+
+    """
+    K = gmm.k
+    D = gmm.d
+    soft_bow = ss.reshape(-1, K + 2 * K * D)
+    return soft_bow[:, :K]
+
+
+def sstats_to_sfv(ss, gmm):
+    """ Computes spatial Fisher vectors from spatial sufficient statistics.
+
+    Inputs
+    ------
+    ss: array [N, K + 2 * 3 * K]
+        Sufficient statistics for all of the N videos in the data set 
+        (training or testing set). The sufficient statistics are obtained
+        by applying the function _compute_spatial_statistics.
+
+    gmm: instance of yael object gmm
+        Gauassian mixture object.
+
+    Output
+    ------
+    fv: array [N, 2 * 3 * K]
+        Fisher vectors for each of the N videos in the data set. 
+
+    """
+    # Initialize parameters.
+    K = gmm.k
+    mm = self.mm[newaxis, :, newaxis]             # 1x3x1
+    S = self.S[newaxis, :, newaxis]               # 1x3x1
+    ss = ss.reshape(-1, K + 2 * 3 * K)
+    N = ss.shape[0]
+
+    # Get each part of the sufficient statistics.
+    Q_sum = ss[:, :K]                             # NxK
+    Q_ll = ss[:, K:K + 3 * K]                     # NxKD
+    Q_ll_2 = ss[:, K + 3 * K:K + 2 * 3 * K]       # NxKD
+
+    # Compute the gradients.
+    d_mm = Q_ll - (Q_sum[:, newaxis] * mm).reshape(N, 3 * K, order='F')
+    d_S = (
+        - Q_ll_2
+        - (Q_sum[:, newaxis] * mm ** 2).reshape(N, 3 * K, order='F')
+        + (Q_sum[:, newaxis] * S).reshape(N, 3 * K, order='F')
+        + 2 * Q_ll * tile(squeeze(mm)[newaxis], K))
+    xx = hstack((d_mm, d_S))
     return xx
