3da08f762acb23024187fe353be2244406a016d6
Dan Oneata <dan.oneata@inria.fr>
2012-06-08 11:51:20 +0200

    Some standard values used in my experiments.

---
+++ constants.py
@@ -0,0 +1,12 @@
+""" Some standard values that can be easily reused across all the functions and
+modules.
+
+"""
+DESCS_LEN = {
+    'mbh': 192,
+    'hog': 96,
+    'hof': 108,
+    'hoghof': 96 + 108,
+    'all': 96 + 108 + 192}
+NR_PCA_COMPONENTS = 64
+IP_TYPE = 'dense5.track15mbh'
