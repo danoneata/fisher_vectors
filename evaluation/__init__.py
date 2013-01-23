00d533a865265d04d506b5d5efdb2cbc0754c375
Dan Oneata <dan.oneata@inria.fr>
2013-01-23 18:52:57 +0100

    Added evaluation for Duchenne 09 and TrecVid 12 datasets.

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -2,3 +2,5 @@ from .factory import Evaluation
 from .hollywood2_evaluation import Hollywood2Evaluation
 from .kth_evaluation import KTHEvaluation
 from .trecvid11_evaluation import TrecVid11Evaluation
+from .trecvid12_evaluation import TrecVid12Evaluation
+from .duch09_evaluation import Duch09Evaluation
