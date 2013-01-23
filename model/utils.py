99e3e878d375ba8a4332d3c3c7d9ae8a9b50000c
Dan Oneata <dan.oneata@inria.fr>
2013-01-23 18:56:54 +0100

    ENH Using square root transformation only on counts.

--- model/utils.py
+++ model/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import warnings
+
+from yael import yael
 
 
 def standardize(xx, mu=None, sigma=None):
@@ -39,7 +42,8 @@ def standardize(xx, mu=None, sigma=None):
         sigma = np.std(xx - mu, 0)
 
     if np.min(sigma) == 0:
-        raise ValueError, "There is zero variance on some dimension."
+        warnings.warn("At least one dimension of the data has zero variance.")
+        sigma[sigma == 0] = 1.
 
     return (xx - mu) / sigma, mu, sigma
 
@@ -82,3 +86,56 @@ def L2_normalize(xx):
     """
     Zx = compute_L2_normalization(xx)
     return xx / np.sqrt(Zx[:, np.newaxis])
+
+
+def sstats_to_sqrt_features(ss, gmm):
+    """ Converts sufficient statistics into an approximated form of sqrt
+    Fisher vectors.
+
+    Inputs
+    ------
+    ss: array [N * (K + 2 * D * K), ]
+        Sufficient statistics.
+
+    gmm: instance of the yael object, gmm
+        Gaussian mixture model.
+
+    Output
+    ------
+    fv: array [N, K + 2 * D * K]
+        Fisher vectors for each of the N sufficient statistics.
+
+    """
+    K = gmm.k
+    D = gmm.d
+    ss = ss.reshape(-1, K + 2 * K * D)
+    N = ss.shape[0]
+
+    # Get parameters and reshape them.
+    pi = yael.fvec_to_numpy(gmm.w, K)            # 1xK
+    mu = (yael.fvec_to_numpy(gmm.mu, K * D)
+          .reshape(D, K, order='F')[np.newaxis])    # 1xDxK
+    sigma = (yael.fvec_to_numpy(gmm.sigma, K * D).
+             reshape(D, K, order='F')[np.newaxis])  # 1xDxK
+
+    # Get each part of the sufficient statistics.
+    Q_sum = ss[:, :K]                            # NxK (then Nx1xK)
+    Q_xx = ss[:, K:K + D * K]                    # NxKD
+    Q_xx_2 = ss[:, K + D * K:K + 2 * D * K]      # NxKD
+
+    # Sqrt the soft counts.
+    sqrtQ = np.sqrt(Q_sum)
+
+    # Compute the gradients.
+    d_pi = sqrtQ
+    d_mu = Q_xx - (Q_sum[:, np.newaxis] * mu).reshape(N, D * K, order='F')
+    d_mu = d_mu / np.repeat(sqrtQ, D)
+    d_sigma = (
+        - Q_xx_2
+        - (Q_sum[:, np.newaxis] * mu ** 2).reshape(N, D * K, order='F')
+        + (Q_sum[:, np.newaxis] * sigma).reshape(N, D * K, order='F')
+        + 2 * Q_xx * mu.reshape(1, K * D, order='F')) / np.repeat(sqrtQ, D)
+
+    # Glue everything together.
+    fv = np.hstack((d_pi, d_mu, d_sigma))
+    return fv
